1a0f82c6ffd20c204536ed90bebcc79d7f0f0470
Aytac <aytacibrahimova2612@gmail.com>
2025-05-03 14:22:20 +0400

    First commit

---
+++ src/test/java/stepdefs/MyStepDefs.java
@@ -0,0 +1,29 @@
+package stepdefs;
+
+import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class MyStepDefs {
+
+    private Response response;
+
+    @Given("I send a GET request to {string}")
+    public void i_send_a_get_request_to(String url) {
+        response = RestAssured.get(url);
+    }
+
+    @Then("the status code should be {int}")
+    public void the_status_code_should_be(int expectedStatusCode) {
+        assertThat(response.getStatusCode(), is(expectedStatusCode));
+    }
+
+    @Then("the response should contain")
+    public void the_response_should_contain(String expectedJson) {
+        String actualJson = response.getBody().asString();
+        assertThat(actualJson.trim(), equalToIgnoringWhiteSpace(expectedJson.trim()));
+    }
+}
